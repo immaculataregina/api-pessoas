a7007204e370563d2169eebd8e950682acff8cd4
Thiago Lamberti <thiago.lsanches2@gmail.com>
2022-12-24 15:36:39 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const indexRouter = require('./routers/index.router');
-const usuarioRouter = require('./routers/usuario.router');
+const pessoasRouter = require('./routers/pessoas.router');
 const app = require('./init.app');
 
 // Router
 app.use('/', indexRouter);
-app.use('/usuario', usuarioRouter);
+app.use('/pessoas', pessoasRouter);
 
 module.exports = app;
